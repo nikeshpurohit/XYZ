9274de330461ad719754b269488485a5480a1854
nikeshpurohit <hello@nikeshpuroh.it>
2019-11-10 16:30:38 +0000

    Some work to query the DB

--- src/java/Servlets/ServletOne.java
+++ src/java/Servlets/ServletOne.java
@@ -8,6 +8,10 @@ package Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -33,15 +37,25 @@ public class ServletOne extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            ResultSet res = Objects.DBConnectionProvider.executeQuery("SELECT * FROM XYZ.\"Users\"");
+            
+            while (res.next()){
+                out.println(res.getString("id"));
+            }
+            
+           
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ServletOne</title>");            
+            out.println("<title>Logon servlet</title>");            
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet ServletOne at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
+        } catch (SQLException ex) {
+            Logger.getLogger(ServletOne.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
