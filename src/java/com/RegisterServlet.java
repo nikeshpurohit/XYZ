d4bc60527ed50cf2d4153492ca37d641855e051a
karlSartorio <karlSartorio@users.noreply.github.com>
2019-11-24 21:37:29 +0000

    Update RegisterServlet.java

--- src/java/com/RegisterServlet.java
+++ src/java/com/RegisterServlet.java
@@ -41,6 +41,7 @@ public class RegisterServlet extends HttpServlet {
             String RUserName = request.getParameter("rUsernameInput");
             String RPassword = request.getParameter("rPasswordInput");
             String RPassword2 = request.getParameter("rPasswordInput2");
+            String Status = "APPLIED";
             
             model.User user = new model.User();
             dao.UsersDAOImpl userDAO = new dao.UsersDAOImpl();
@@ -73,15 +74,11 @@ public class RegisterServlet extends HttpServlet {
                     {
                         out.println("username and password is added to the database and direct you to the members dashboard");
                         request.getSession().setAttribute("RegisterError", "none");
-                        request.getRequestDispatcher("/XYZ/Login.jsp").forward(request, response);
-                        
-                        
-                        
-                        model.LoginSession login_session = new model.LoginSession(user, session, response);
+                        model.User AddUser = new model.User(RUserName, RPassword, Status);
+                        dao.UsersDAOImpl.createNewUser(AddUser);
+                        request.getRequestDispatcher("/XYZ/Login.jsp").forward(request, response);  
+                       
 
-                        session.setAttribute("login_session", login_session);
-
-                        if (login_session.validateUser(RUserName, RPassword));{request.getRequestDispatcher("UserDashServlet");} 
                     } 
                     }
                     /*else {
