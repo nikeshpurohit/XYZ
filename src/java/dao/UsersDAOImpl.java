24250abf83046ca9fe37fc6499eb0acdb2689f7a
nikeshpurohit <hello@nikeshpuroh.it>
2019-11-24 20:47:09 +0000

    Update UserDAOImpl create a prepared statement

--- src/java/dao/UsersDAOImpl.java
+++ src/java/dao/UsersDAOImpl.java
@@ -5,6 +5,7 @@
  */
 package dao;
 
+import java.sql.Connection;
 import model.DBConnectionProvider;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -70,7 +71,10 @@ public class UsersDAOImpl {
         String status = user.getStatus();
         
         //DB Query
-        //String query = "INSERT INTO XYZ.\"Users\"";
+        String query = "INSERT INTO XYZ.\"Users\" (\"id\",\"password\",\"status\") VALUES (\"" + id + "\", \"" + password + "\", \"" + status + "\")" ;
+        System.out.println(query);
+        
+                
     }
 
 }
